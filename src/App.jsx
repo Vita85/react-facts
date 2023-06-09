597936dac111e46ab77108d18db82d4fdca9b69c
VitaNomSmile <VNomerchyk@gmail.com>
2023-06-09 21:22:21 +0300

    Change mode on dark or light

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,38 @@
-import logo from "./img/Group.svg"
-import logo_promo from "./img/reactjs-icon 2.png"
-
+import logo from "./img/Group.svg";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  }
   return (
     <div className="App">
-      <div className="container">
-
+      <div className={darkMode ? "container" : "container light"}>
         <div className="header">
-          <img src={logo} alt="Logo" className="logo__img" />
-          <div className="logo__text">ReactFacts</div>
+          <div className="logo">
+            <img src={logo} alt="Logo" className="logo__img" />
+            <div className="logo__text">ReactFacts</div>
+          </div>
+          <div className="toggler">
+            <div className="toggler__circle" onClick={toggleDarkMode}></div>
+          </div>
         </div>
 
         <div className="box__facts">
           <h2>Fun facts about React</h2>
           <div className="promo__items">
-          <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-          </ul>
+            <ul>
+              <li>Was first released in 2013</li>
+              <li>Was originally created by Jordan Walke</li>
+              <li>Has well over 100K stars on GitHub</li>
+              <li>Is maintained by Facebook</li>
+              <li>
+                Powers thousands of enterprise apps, including mobile apps
+              </li>
+            </ul>
           </div>
-          <img src={logo_promo} alt="Logo" className="promo__img" />
-
         </div>
-
       </div>
     </div>
   );
